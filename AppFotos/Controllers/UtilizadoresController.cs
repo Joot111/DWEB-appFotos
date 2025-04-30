2d60ea1405a097e1257adc428459a7fbea8defa8
Joot111 <joaoamoroso04@gmail.com>
2025-04-30 09:44:07 +0100

    autorização de acesso aos dados dos Utilizadores

--- AppFotos/Controllers/UtilizadoresController.cs
+++ AppFotos/Controllers/UtilizadoresController.cs
@@ -7,9 +7,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AppFotos.Data;
 using AppFotos.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace AppFotos.Controllers
 {
+    [Authorize] // apenas pessoas autenticadas têm acesso à página de apresentação dos utilizadores
     public class UtilizadoresController : Controller
     {
         /// <summary>
@@ -51,31 +53,6 @@ namespace AppFotos.Controllers
                 return NotFound();
             }
 
-            return View(utilizador);
-        }
-
-        // GET: Utilizadores/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Utilizadores/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,Morada,CodPostal,Pais,NIF,Telemovel")] Utilizadores utilizador)
-        {
-            if (ModelState.IsValid)
-            {
-                // Corrigir os dados do Código Postal
-                utilizador.CodPostal=utilizador.CodPostal.ToUpper();
-
-                _context.Add(utilizador);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
             return View(utilizador);
         }
 
